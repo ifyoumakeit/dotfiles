499bbfb7da09a1182c12edb6c4cbd591fddc2731
Kevin Pruett <pruett.kevin@gmail.com>
2017-06-05 11:07:16 -0400

    update

--- config.dotfile/fish/config.fish
+++ config.dotfile/fish/config.fish
@@ -1,4 +1,11 @@
-set -x PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/MacGPG2/bin ~/.config/yarn/global/node_modules/.bin
+# PATH
+set -x PATH /usr/local/bin\
+						/usr/bin\
+						/bin\
+						/usr/sbin\
+						/sbin\
+						/usr/local/MacGPG2/bin\
+						~/.config/yarn/global/node_modules/.bin/
 
 # System
 alias reload!="source ~/.config/fish/config.fish"
@@ -12,3 +19,6 @@ alias tmuxn="tmux new -As"
 alias tmuxa="tmux attach -t"
 alias tmuxk="tmux kill-session -t"
 alias tmuxl='tmux list-sessions'
+
+# Git
+alias gca="git commit -a"
